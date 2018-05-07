341543aa4b036c2ba2f080f9a4d3e133022c2135
nuwan rathnayaka <nuwanrathnayaka.c@gmail.com>
2018-05-07 21:36:07 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,70 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { FootComponent } from './shared/foot/foot.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TablesComponent } from './components/tables/tables.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    AboutComponent,
+    NotfoundComponent,
+    FootComponent,
+    SidebarComponent,
+    TablesComponent,
+    DashboardComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+    RouterModule.forRoot([
+
+       { path:'', 
+         component:HomeComponent
+       },
+    //   { path:'', 
+    //   component:SidebarComponent
+    // },
+      { path:'home', 
+         component:HomeComponent
+       },
+      { path:'about', 
+        component:AboutComponent
+      },
+      { path:'contact', 
+        component:ContactComponent
+      },
+      { path:'sidebar', 
+        component:SidebarComponent,
+      
+        children:[
+          {path:'dashboard', component:DashboardComponent},
+          {path:'tables', component:TablesComponent}
+        ]
+      },
+      { path:'**', 
+        component:NotfoundComponent
+      }
+
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
